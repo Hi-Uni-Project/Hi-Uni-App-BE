55046040091058823e169e479606d3ab5f0af12c
june <0721hjp@naver.com>
2025-05-09 22:37:45 +0900

    fix: cors 오류 수정, 인코딩 문제 수정

--- src/main/java/com/qoormthon/todool/global/config/WebConfig.java
+++ src/main/java/com/qoormthon/todool/global/config/WebConfig.java
@@ -9,7 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://localhost:3000"
+                )
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
