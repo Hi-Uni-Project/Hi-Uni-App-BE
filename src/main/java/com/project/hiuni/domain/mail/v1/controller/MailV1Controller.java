e6dc1fa05e2917e220f6748e46f4df95759028d9
june <0721hjp@naver.com>
2025-06-18 10:43:00 +0900

    init: 초기세팅

--- src/main/java/com/project/hiuni/domain/mail/v1/controller/MailV1Controller.java
+++ src/main/java/com/project/hiuni/domain/mail/v1/controller/MailV1Controller.java
@@ -1,5 +1,10 @@
 package com.project.hiuni.domain.mail.v1.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mail")
 public class MailV1Controller {
 
 }
