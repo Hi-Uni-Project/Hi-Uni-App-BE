d9be332c6c3468f7a09255455f6919b4482567f7
choizz156 <tkfka156@gmail.com>
2025-06-29 11:41:18 +0900

    docs: swagger api 문서 생성 코드 작성

--- src/main/java/com/project/hiuni/admin/domain/terms/controller/IdentityVerificationController.java
+++ src/main/java/com/project/hiuni/admin/domain/terms/controller/IdentityVerificationController.java
@@ -5,6 +5,8 @@ import com.project.hiuni.admin.domain.terms.dto.TermsResponseDto;
 import com.project.hiuni.admin.domain.terms.entity.IdentityVerification;
 import com.project.hiuni.admin.domain.terms.service.IdentityVerificationService;
 import com.project.hiuni.global.common.dto.response.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 가입 동의 약관 추가 및 조회 api", description = "회원 가입 동의 약관을 추가 및 조회 할 수 있습니다. admin 권한이 필요합니다.")
 @RequiredArgsConstructor
 @RequestMapping("/admin/identity-terms")
 @RestController
@@ -20,6 +23,7 @@ public class IdentityVerificationController {
 
 	private final IdentityVerificationService identityVerificationService;
 
+	@Operation(summary = "버전 별로 회원 가입 동의 약관을 조회할 수 있습니다.")
 	@GetMapping("/{version}")
 	public ResponseDto<TermsResponseDto> findByVersion(@PathVariable String version) {
 
@@ -35,6 +39,7 @@ public class IdentityVerificationController {
 		return ResponseDto.response(termsResponseDto);
 	}
 
+	@Operation(summary = "가장 최근 버전의 회원 가입 동의 약관을 조회할 수 있습니다.")
 	@GetMapping
 	public ResponseDto<TermsResponseDto> findLatest() {
 
@@ -50,6 +55,7 @@ public class IdentityVerificationController {
 		return ResponseDto.response(termsResponseDto);
 	}
 
+	@Operation(summary = "회원 가입 동의 약관을 추가할 수 있습니다.")
 	@PostMapping
 	public void create(@RequestBody TermsRequestDto termsRequestDto) {
 		identityVerificationService.create(
--- src/main/java/com/project/hiuni/admin/domain/terms/controller/ServiceImprovementTermsController.java
+++ src/main/java/com/project/hiuni/admin/domain/terms/controller/ServiceImprovementTermsController.java
@@ -5,6 +5,8 @@ import com.project.hiuni.admin.domain.terms.dto.TermsResponseDto;
 import com.project.hiuni.admin.domain.terms.entity.ServiceImprovementTerms;
 import com.project.hiuni.admin.domain.terms.service.ServiceImprovementTermsManager;
 import com.project.hiuni.global.common.dto.response.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "서비스 개선 약관 추가 및 조회 api", description = "서비스 개선 약관을 추가 및 조회 할 수 있습니다. admin 권한이 필요합니다.")
 @RequiredArgsConstructor
 @RequestMapping("/admin/service-improvements-terms")
 @RestController
@@ -20,6 +23,7 @@ public class ServiceImprovementTermsController {
 
 	private final ServiceImprovementTermsManager ServiceImprovementTermsManager;
 
+	@Operation(summary = "버전 별로 서비스 개선 약관을 조회할 수 있습니다.")
 	@GetMapping("/{version}")
 	public ResponseDto<TermsResponseDto> findByVersion(@PathVariable String version) {
 
@@ -34,6 +38,7 @@ public class ServiceImprovementTermsController {
 		return ResponseDto.response(termsResponseDto);
 	}
 
+	@Operation(summary = "버전 별로 서비스 개선 약관을 조회할 수 있습니다.")
 	@GetMapping
 	public ResponseDto<TermsResponseDto> findLatest() {
 
@@ -48,6 +53,7 @@ public class ServiceImprovementTermsController {
 		return ResponseDto.response(termsResponseDto);
 	}
 
+	@Operation(summary = "버전 별로 서비스 개선 약관을 조회할 수 있습니다.")
 	@PostMapping
 	public void create(@RequestBody TermsRequestDto termsRequestDto) {
 
