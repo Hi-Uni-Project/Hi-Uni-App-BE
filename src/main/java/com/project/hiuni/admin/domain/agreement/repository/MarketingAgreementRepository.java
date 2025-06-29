09b3e224b3ced5f0a8c5f99233eb32dc74aab076
choizz156 <tkfka156@gmail.com>
2025-06-29 20:46:49 +0900

    feat: 약관 동의 레포지토리 생성

---
+++ src/main/java/com/project/hiuni/admin/domain/agreement/repository/MarketingAgreementRepository.java
@@ -0,0 +1,8 @@
+package com.project.hiuni.admin.domain.agreement.repository;
+
+import com.project.hiuni.admin.domain.agreement.entity.MarketingAgreement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarketingAgreementRepository extends JpaRepository<MarketingAgreement, Long> {
+
+}
