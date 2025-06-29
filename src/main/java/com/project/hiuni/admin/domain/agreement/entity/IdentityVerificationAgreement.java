d7281aec5e38961347a6f17d1e25afafb9db8659
choizz156 <tkfka156@gmail.com>
2025-06-29 19:01:22 +0900

    feat: 약관 내역 동의 관련 엔티티 생성

---
+++ src/main/java/com/project/hiuni/admin/domain/agreement/entity/IdentityVerificationAgreement.java
@@ -0,0 +1,29 @@
+package com.project.hiuni.admin.domain.agreement.entity;
+
+import com.project.hiuni.admin.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "marketing_subs_agreement")
+@Entity
+public class IdentityVerificationAgreement extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long userId;
+
+	private Long personalInfoTermId;
+
+	private LocalDateTime agreedAt;
+}
