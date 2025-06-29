1d76727b028f7a1fb9ddc5ba36e12b6d8b17fb97
choizz156 <tkfka156@gmail.com>
2025-06-29 11:13:06 +0900

    feat: ServiceTerms 조회 로직 작성

---
+++ src/main/java/com/project/hiuni/admin/domain/terms/controller/ServiceTermsController.java
@@ -0,0 +1,60 @@
+package com.project.hiuni.admin.domain.terms.controller;
+
+import com.project.hiuni.admin.domain.terms.dto.TermsRequestDto;
+import com.project.hiuni.admin.domain.terms.dto.TermsResponseDto;
+import com.project.hiuni.admin.domain.terms.entity.ServiceTerms;
+import com.project.hiuni.admin.domain.terms.service.ServiceTermsManager;
+import com.project.hiuni.global.common.dto.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin/service-terms")
+@RestController
+public class ServiceTermsController {
+
+	private final ServiceTermsManager serviceTermsManager;
+
+	@GetMapping("/{version}")
+	public ResponseDto<TermsResponseDto> findByVersion(@PathVariable String version) {
+
+		ServiceTerms serviceTerms = serviceTermsManager.getByVersion(version);
+
+		TermsResponseDto termsResponseDto = new TermsResponseDto(
+			serviceTerms.getContents(),
+			serviceTerms.getVersion(),
+			serviceTerms.getEffectiveDate()
+		);
+
+		return ResponseDto.response(termsResponseDto);
+	}
+
+	@GetMapping
+	public ResponseDto<TermsResponseDto> findLatest() {
+
+		ServiceTerms serviceTerms = serviceTermsManager.getByLastest();
+
+		TermsResponseDto termsResponseDto = new TermsResponseDto(
+			serviceTerms.getContents(),
+			serviceTerms.getVersion(),
+			serviceTerms.getEffectiveDate()
+		);
+
+		return ResponseDto.response(termsResponseDto);
+	}
+
+	@PostMapping
+	public void create(@RequestBody TermsRequestDto termsRequestDto) {
+
+		serviceTermsManager.create(
+			termsRequestDto.content(),
+			termsRequestDto.version(),
+			termsRequestDto.effectiveDate()
+		);
+	}
+}
--- src/main/java/com/project/hiuni/admin/domain/terms/service/ServiceTermsManager.java
+++ src/main/java/com/project/hiuni/admin/domain/terms/service/ServiceTermsManager.java
@@ -3,7 +3,9 @@ package com.project.hiuni.admin.domain.terms.service;
 import com.project.hiuni.admin.domain.terms.entity.ServiceTerms;
 import com.project.hiuni.admin.domain.terms.entity.TermsInfo;
 import com.project.hiuni.admin.domain.terms.repository.ServiceTermsRepository;
+import com.project.hiuni.global.exception.ErrorCode;
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +22,13 @@ public class ServiceTermsManager {
 		serviceTermsRepository.save(serviceTerms);
 	}
 
+	public ServiceTerms getByVersion(String version) {
+		return serviceTermsRepository.findIdentityVerificationByTermsInfo_AgreeVersion(version)
+			.orElseThrow(() -> new NoSuchElementException(ErrorCode.NOT_FOUND.getMessage() + ":" + version));
+	}
+
+	public ServiceTerms getByLastest() {
+		return serviceTermsRepository.findTopByOrderByCreatedAtDesc()
+			.orElseThrow(() -> new NoSuchElementException(ErrorCode.NOT_FOUND.getMessage()));
+	}
 }
