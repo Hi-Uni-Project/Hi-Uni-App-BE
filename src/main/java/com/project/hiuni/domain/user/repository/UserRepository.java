234bf29d1de9a9fa87900aafcb44b9dda04fe6a9
june <0721hjp@naver.com>
2025-07-01 16:43:47 +0900

    feat: CustomUserDetailsService 구현

--- src/main/java/com/project/hiuni/domain/user/repository/UserRepository.java
+++ src/main/java/com/project/hiuni/domain/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.project.hiuni.domain.user.repository;
 
 import com.project.hiuni.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findBySocialEmail(String socialEmail);
 
 }
--- src/main/java/com/project/hiuni/global/security/core/CustomUserDetailsService.java
+++ src/main/java/com/project/hiuni/global/security/core/CustomUserDetailsService.java
@@ -1,12 +1,23 @@
 package com.project.hiuni.global.security.core;
 
+import com.project.hiuni.domain.user.entity.User;
+import com.project.hiuni.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
-  private final
+  private final UserRepository userRepository;
 
+  @Override
+  public UserDetails loadUserByUsername(String socialEmail) throws UsernameNotFoundException {
+    User user = userRepository.findBySocialEmail(socialEmail).orElseThrow(
+        () -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    return new CustomUserDetails(user);
+  }
 }
