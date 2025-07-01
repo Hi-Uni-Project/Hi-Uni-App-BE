fdf35072c90fe9c2f19a5d14dd0536879d4957c5
june <0721hjp@naver.com>
2025-07-01 16:49:15 +0900

    chore: CustomUserNotFoundException 생성 - user->exception 패키지 내부에 생성 - CustomuserDetailsService -> loadUserByUsername 메서드에서 소셜 이메일로 유저 조회 실패 시 발생

--- src/main/java/com/project/hiuni/global/security/core/CustomUserDetailsService.java
+++ src/main/java/com/project/hiuni/global/security/core/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package com.project.hiuni.global.security.core;
 
 import com.project.hiuni.domain.user.entity.User;
+import com.project.hiuni.domain.user.exception.CustomUserNotFoundException;
 import com.project.hiuni.domain.user.repository.UserRepository;
+import com.project.hiuni.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,10 +16,17 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
+  /**
+   * 사용자 인증 객체를 생성하는 메서드입니다.
+   *
+   * @param username 사용자 식별자인 소셜 이메일 값이 들어갑니다.
+   * @return CustomUserDetails 객체
+   * @throws UsernameNotFoundException 사용자를 찾을 수 없는 경우
+   */
   @Override
-  public UserDetails loadUserByUsername(String socialEmail) throws UsernameNotFoundException {
-    User user = userRepository.findBySocialEmail(socialEmail).orElseThrow(
-        () -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findBySocialEmail(username).orElseThrow(
+        () -> new CustomUserNotFoundException(ErrorCode.USER_NOT_FOUND));
     return new CustomUserDetails(user);
   }
 }
