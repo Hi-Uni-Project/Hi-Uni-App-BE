241fcd46bb27cc1a029d64ca36242349d7b2c988
june <0721hjp@naver.com>
2025-07-01 17:49:25 +0900

    feat: jwtTokenFilter 등록

--- src/main/java/com/project/hiuni/global/security/config/SecurityConfig.java
+++ src/main/java/com/project/hiuni/global/security/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.project.hiuni.global.security.config;
 
+import com.project.hiuni.global.security.jwt.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final JwtTokenFilter jwtTokenFilter;
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
@@ -25,9 +33,9 @@ public class SecurityConfig {
             .requestMatchers(
                 "/swagger-ui/**",
                 "/v3/api-docs/**"
-            )
-            .permitAll()
-            .anyRequest().denyAll());
+            ).permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
