4cd9232ad6bf788f1f4485d13608072b2d8ea3bb
june <0721hjp@naver.com>
2025-07-01 13:36:49 +0900

    refactor: 공통 응답 객체 통일

--- src/main/java/com/project/hiuni/global/common/dto/response/ResponseDto.java
+++ src/main/java/com/project/hiuni/global/common/dto/response/ResponseDto.java
@@ -1,22 +1,30 @@
 package com.project.hiuni.global.common.dto.response;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
-public class
-ResponseDto<T>  {
-    private HttpStatus status;
-    private String message;
-    private T data;
+public class ResponseDto<T> {
 
-    public static <T> ResponseDto<T> response(HttpStatus status, String message, T data) {
-        ResponseDto<T> responseDto = new ResponseDto<>();
-        responseDto.setStatus(status);
-        responseDto.setMessage(message);
-        responseDto.setData(data);
-        return responseDto;
-    }
+  private LocalDateTime timestamp = LocalDateTime.now();
+  private HttpStatus status;
+  private String message;
+  private T data;
+
+  public static <T> ResponseDto<T> response(HttpStatus status, String message, T data) {
+    ResponseDto<T> responseDto = new ResponseDto<>();
+    responseDto.setStatus(status);
+    responseDto.setMessage(message);
+    responseDto.setData(data);
+    return responseDto;
+  }
+
+  public static <T> ResponseDto<T> response(T data) {
+    ResponseDto<T> responseDto = new ResponseDto<>();
+    responseDto.setData(data);
+    return responseDto;
+  }
 }
