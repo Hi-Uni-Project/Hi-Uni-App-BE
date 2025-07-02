7fb02cb3f0e2874c3bff6210cee596215130895a
june <0721hjp@naver.com>
2025-07-02 14:19:21 +0900

    refactor: User 엔티티 @setter 어노테이션 삭제

--- src/main/java/com/project/hiuni/domain/user/entity/User.java
+++ src/main/java/com/project/hiuni/domain/user/entity/User.java
@@ -12,12 +12,10 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 @Entity
 
 public class User extends BaseEntity {
