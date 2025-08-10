d0d44e99dc697015b63985f242e1901c796deeca
june <0721hjp@naver.com>
2025-08-10 22:45:39 +0900

    test: jwt 엑세스, 리프레시 토큰 생성 테스트코드 추가

--- src/test/java/com/project/hiuni/global/security/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/project/hiuni/global/security/jwt/JwtTokenProviderTest.java
@@ -2,16 +2,23 @@ package com.project.hiuni.global.security.jwt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.project.hiuni.domain.user.entity.ProfileImage;
 import com.project.hiuni.domain.user.entity.User;
+import com.project.hiuni.domain.user.entity.UserStatus;
+import com.project.hiuni.domain.user.v1.service.SocialProvider;
 import com.project.hiuni.global.security.core.CustomUserDetails;
+import com.project.hiuni.global.security.core.Role;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import javax.crypto.SecretKey;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -22,17 +29,35 @@ class JwtTokenProviderTest {
   private SecretKey secretKey;
 
   @Test
+  @DisplayName("JWT 엑세스 토큰을 생성한다.")
   void createAccessToken() {
     //given
+    User testUser = getTestUser();
+    Authentication authentication = getTestAuthentication(testUser);
+
+    Long accessTokenExpirationTime = 900000L; // 15 minutes in milliseconds
+
     //when
+    String accessToken = createToken(authentication, accessTokenExpirationTime);
+
     //then
+    assertNotNull(accessToken);
   }
 
   @Test
+  @DisplayName("JWT 리프레시 토큰을 생성한다.")
   void createRefreshToken() {
     //given
+    User testUser = getTestUser();
+    Authentication authentication = getTestAuthentication(testUser);
+
+    Long refreshTokenExpirationTime = 2592000000L; // 30 days in milliseconds
+
     //when
+    String refreshToken = createToken(authentication, refreshTokenExpirationTime);
+
     //then
+    assertNotNull(refreshToken);
   }
 
 
@@ -47,6 +72,44 @@ class JwtTokenProviderTest {
         .expiration(expiryDate)
         .signWith(secretKey, SignatureAlgorithm.HS512)
         .compact();
+  }
+
+  /**
+   * 테스트용 Authentication 객체를 생성하는 메서드입니다.
+   *
+   * @return Authentication 객체
+   */
+  public Authentication getTestAuthentication(User user) {
+    UserDetails userTestDetails = new CustomUserDetails(user);
+    return new UsernamePasswordAuthenticationToken(
+        userTestDetails,
+        null,
+        userTestDetails.getAuthorities()
+    );
+
+  }
+
+  /**
+   * 테스트용 User 객체를 생성하는 메서드입니다.
+   *
+   * @return User 객체
+   */
+  public User getTestUser() {
+    return User.builder()
+        .id(1L)
+        .socialEmail("test@kakao.com")
+        .socialProvider(SocialProvider.KAKAO)
+        .univName("Test University")
+        .majorName("Computer Science")
+        .univEmail("test@test.ac.kr")
+        .nickname("test")
+        .imageUrl("testImageUrl")
+        .role(Role.ROLE_USER)
+        .status(UserStatus.ACTIVE)
+        .marketingConsent(true)
+        .improvementConsent(true)
+        .profileImage(ProfileImage.of("testImageUrl", "testImageUrl", null))
+        .build();
   }
 
 
