fb86e5968ceea82983982c60247589242587dff0
june <0721hjp@naver.com>
2025-08-12 22:27:31 +0900

    chore: 유효성 검증 예외처리를 위한 커스텀 익셉션 클래스 추가 - ValidationException

--- src/main/java/com/project/hiuni/domain/auth/dto/request/AuthSocialRequest.java
+++ src/main/java/com/project/hiuni/domain/auth/dto/request/AuthSocialRequest.java
@@ -1,6 +1,7 @@
 package com.project.hiuni.domain.auth.dto.request;
 
 import com.project.hiuni.domain.auth.entity.SocialProvider;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,10 @@ import lombok.Setter;
 @Setter
 @Builder
 public class AuthSocialRequest {
+
+  @NotNull(message = "ID 토큰은 필수입니다.")
   private String idToken;
+
+  @NotNull(message = "소셜 provider는 필수입니다.")
   private SocialProvider provider;
 }
--- src/main/java/com/project/hiuni/domain/auth/v1/controller/AuthController.java
+++ src/main/java/com/project/hiuni/domain/auth/v1/controller/AuthController.java
@@ -3,17 +3,30 @@ package com.project.hiuni.domain.auth.v1.controller;
 import com.project.hiuni.domain.auth.docs.AuthApiDocumentation;
 import com.project.hiuni.domain.auth.dto.request.AuthSocialRequest;
 import com.project.hiuni.domain.auth.dto.response.AuthSocialResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/auth")
 public class AuthController implements AuthApiDocumentation {
 
   @PostMapping("/social")
-  public AuthSocialResponse authSocial(@RequestBody AuthSocialRequest authSocialRequest) {
+  public AuthSocialResponse authSocial(
+      @RequestBody @Valid AuthSocialRequest authSocialRequest,
+      BindingResult bindingResult) {
+
+    if(bindingResult.hasErrors()) {
+      log.error("AuthSocialRequest validation failed :: {}", bindingResult.getAllErrors());
+
+    }
+
 
   }
 
