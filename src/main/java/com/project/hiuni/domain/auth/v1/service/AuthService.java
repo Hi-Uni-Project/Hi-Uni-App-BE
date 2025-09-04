f27892c124bf0b2bcda7084dc5f08c7ed374f15a
june <0721hjp@naver.com>
2025-09-04 21:50:10 +0900

    chore: json 파싱을 위한 의존성 추가 - com.google.api-client:google-api-client-jackson2:2.4.0

--- src/main/java/com/project/hiuni/domain/auth/v1/service/AuthService.java
+++ src/main/java/com/project/hiuni/domain/auth/v1/service/AuthService.java
@@ -7,6 +7,7 @@ import com.project.hiuni.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
@@ -14,13 +15,18 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
   private final JwtTokenProvider jwtTokenProvider;
-
+  private final RestTemplate restTemplate;
 
 
   public AuthSocialResponse socialLogin(AuthSocialRequest authSocialRequest) {
     SocialProvider userProvider = authSocialRequest.getProvider();
+
+    // 소셜 플랫폼이 구글일 경우
     if(userProvider == SocialProvider.GOOGLE) {
       log.info("Google social login");
+
+      String url =
+
 
 
     }
