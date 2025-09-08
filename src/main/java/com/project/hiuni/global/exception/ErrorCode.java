5253be742156c18074617e61d7004b7f2d3bd145
june <0721hjp@naver.com>
2025-09-09 03:19:54 +0900

    chore: 대학 관련 api 주석문구 수정 및 에러 응답 메세지 수정

--- src/main/java/com/project/hiuni/global/exception/ErrorCode.java
+++ src/main/java/com/project/hiuni/global/exception/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
 
     // 400 Bad Request 검색 관련
     INVALID_SEARCH_KEYWORD_LENGTH(HttpStatus.BAD_REQUEST, "INVALID_SEARCH_KEYWORD_LENGTH", "검색어는 2자 이상 입력해주세요."),
-    INVALID_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, "INVALID_SEARCH_KEYWORD", "검색어에 '대학', '대학교', '학교'는 포함될 수 없습니다."),
+    INVALID_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, "INVALID_SEARCH_KEYWORD", "검색어에 '대학', '대학교', '학교'는 단독으로 사용할 수 없습니다."),
 
     // 400 Bad Request 파일 관련 에러코드
     UNSUPPORTED_FILE_FORMAT(HttpStatus.BAD_REQUEST, "UNSUPPORTED_FILE_FORMAT", "지원하지 않는 파일 형식입니다."),
