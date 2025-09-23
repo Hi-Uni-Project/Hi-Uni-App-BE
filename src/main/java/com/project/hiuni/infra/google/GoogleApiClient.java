a5e9326c7ba66b49d92b6db02c0a5b204e919cf2
june <0721hjp@naver.com>
2025-09-23 18:17:54 +0900

    chore: google id 토큰 검증 대상 clientId 추가 - web 클라이언트 Id로 서버는 검증해야했었음

--- src/main/java/com/project/hiuni/infra/google/GoogleApiClient.java
+++ src/main/java/com/project/hiuni/infra/google/GoogleApiClient.java
@@ -24,9 +24,10 @@ public class GoogleApiClient {
 
   public GoogleApiClient(
       @Value("${oauth.google.client-id-android}") String clientId1,
-      @Value("${oauth.google.client-id-apple}") String clientId2) {
+      @Value("${oauth.google.client-id-apple}") String clientId2,
+      @Value("${oauth.google.client-id-web}") String clientId3) {
 
-    List<String> clientIds = List.of(clientId1, clientId2);
+    List<String> clientIds = List.of(clientId1, clientId2, clientId3);
 
     this.verifier = new GoogleIdTokenVerifier.Builder(
         new NetHttpTransport(),
