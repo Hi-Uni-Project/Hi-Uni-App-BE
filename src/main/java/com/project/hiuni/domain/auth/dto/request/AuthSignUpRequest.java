e9578a82496e8efbd4cb7f3eabe612ec55c61bc6
june <0721hjp@naver.com>
2025-09-29 14:00:19 +0900

    feat: 회원가입 api 요청객체 추가

--- src/main/java/com/project/hiuni/domain/auth/dto/request/AuthSignUpRequest.java
+++ src/main/java/com/project/hiuni/domain/auth/dto/request/AuthSignUpRequest.java
@@ -1,5 +1,56 @@
 package com.project.hiuni.domain.auth.dto.request;
 
+import com.project.hiuni.domain.auth.entity.SocialProvider;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthSignUpRequest {
+
+  @NotNull
+  private Tos tos;
+
+  @NotNull
+  private Univ univ;
+
+  @Getter
+  @Setter
+  public static class Tos {
+    @NotNull(message = "약관 동의 여부는 필수입니다.")
+    private Boolean serviceTosIsAgreed;
+    @NotNull(message = "개인정보 처리방침 동의 여부는 필수입니다.")
+    private Boolean personalInfoTosIsAgreed;
+    @NotNull(message = "마케팅 정보 수신 동의 여부는 필수입니다.")
+    private Boolean marketingTosIsAgreed;
+    @NotNull(message = "서비스 개선을 위한 정보 수신 동의 여부는 필수입니다.")
+    private Boolean serviceImprovementTosIsAgreed;
+    @NotNull(message = "본인 정보로 가입했는지 여부는")
+    private Boolean inPersonTosIsAgreed;
+  }
+
+  @Getter
+  @Setter
+  public static class Univ {
+    @NotNull(message = "대학교 이름은 필수입니다.")
+    private String univName;
+
+    @NotNull(message = "학과 이름은 필수입니다.")
+    private String majorName;
+
+    @NotNull(message = "학교 이메일은 필수입니다.")
+    private String univEmail;
+  }
+
+  @Getter
+  @Setter
+  public static class Social {
+    @NotNull(message = "토큰은 필수입니다.")
+    private String authToken;
+
+    @NotNull(message = "소셜 provider는 필수입니다.")
+    private SocialProvider provider;
+  }
 
 }
