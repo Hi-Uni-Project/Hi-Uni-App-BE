7b4def74cdb5f044fa3d1e429d471c93fdb555cf
Hun0914 <bkh010914@naver.com>
2025-09-29 20:32:16 +0900

    feat : 게시글 좋아요에 대한 Entity, Repository 생성

---
+++ src/main/java/com/project/hiuni/domain/post/repository/PostLikeRepository.java
@@ -0,0 +1,23 @@
+package com.project.hiuni.domain.post.repository;
+
+import com.project.hiuni.domain.post.entity.PostLike;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    //특정 유저가 특정 게시글 좋아요 눌렀는지 확인
+    @Query("SELECT (COUNT(pl) > 0) " +
+            "FROM PostLike pl " +
+            "WHERE pl.post.id = :postId AND pl.user.id = :userId")
+    boolean existsByPostIdAndUserId(@Param("postId") Long postId,
+                                    @Param("userId") Long userId);
+
+    //정합성 검사용 좋아요 전체 숫자 계산
+    @Query("SELECT COUNT(pl) " +
+            "FROM PostLike pl " +
+            "WHERE pl.post.id = :postId")
+    int countPostLikeByPostId(@Param("postId") Long postId);
+}
