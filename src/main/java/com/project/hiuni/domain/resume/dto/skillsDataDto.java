75669135ad5206ebace85ef640979442a1b9bfe3
june <0721hjp@naver.com>
2025-11-11 15:34:59 +0900

    chore: json 매핑 객체 추가

---
+++ src/main/java/com/project/hiuni/domain/resume/dto/skillsDataDto.java
@@ -0,0 +1,13 @@
+package com.project.hiuni.domain.resume.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class skillsDataDto {
+    @JsonProperty("id")
+    private Long skillId;
+    private String name;
+}
