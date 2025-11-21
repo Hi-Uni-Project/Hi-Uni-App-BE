46e36cd9b9207a2e5bd1b09691442b3f8ff3afe9
june <0721hjp@naver.com>
2025-11-21 19:27:42 +0900

    refactor: 소셜 로그인 응답에 user 데이터 추가 + tos 필드 추가 - 현재 유저의 약관 동의 내역 포함

--- src/main/java/com/project/hiuni/domain/tos/repository/InPersonTosHistoryRepository.java
+++ src/main/java/com/project/hiuni/domain/tos/repository/InPersonTosHistoryRepository.java
@@ -1,8 +1,9 @@
 package com.project.hiuni.domain.tos.repository;
 
 import com.project.hiuni.domain.tos.entity.InPersonTosHistory;
+import com.project.hiuni.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InPersonTosHistoryRepository extends JpaRepository<InPersonTosHistory, Long> {
-
+  boolean existsByUser(User user);
 }
