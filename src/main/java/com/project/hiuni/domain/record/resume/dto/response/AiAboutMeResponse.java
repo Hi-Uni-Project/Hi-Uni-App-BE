1f8a1526c3923f3284955775f0a255e7b81248f1
june <0721hjp@naver.com>
2025-11-23 19:27:48 +0900

    feat: AI 내 소개 생성 API 구현

--- src/main/java/com/project/hiuni/domain/record/resume/dto/response/AiAboutMeResponse.java
+++ src/main/java/com/project/hiuni/domain/record/resume/dto/response/AiAboutMeResponse.java
@@ -1,5 +1,14 @@
 package com.project.hiuni.domain.record.resume.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class AiAboutMeResponse {
+
+  private String aboutMe;
 
 }
