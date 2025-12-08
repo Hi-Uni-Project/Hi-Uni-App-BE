698d31e18e294479c53f1ab29a41b5c88c7e9130
june <0721hjp@naver.com>
2025-12-08 13:30:52 +0900

    refactor: 이력서 저장 시 이미지가 업데이트 되었는지 확인하는 필드 추가

--- src/main/java/com/project/hiuni/domain/record/resume/v1/service/ResumeService.java
+++ src/main/java/com/project/hiuni/domain/record/resume/v1/service/ResumeService.java
@@ -89,7 +89,10 @@ public class ResumeService {
       }
 
       // 이력서 기본정보 업데이트
-      resume.updateImageUrl(imageUrl);
+      if(request.getUpdateImage()) { //이미지 변경 필드가 true 일 경우에만 반영
+        resume.updateImageUrl(imageUrl);
+      }
+
       resume.updateName(request.getName());
       resume.updateGender(request.getGender());
       resume.updateBirthYear(request.getBirthYear());
