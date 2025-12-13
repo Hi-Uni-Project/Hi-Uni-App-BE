37f56db116f308c461961a637a3309f1069e6d6e
june <0721hjp@naver.com>
2025-12-13 13:32:19 +0900

    chore: /token-test API 삭제

--- src/main/java/com/project/hiuni/domain/user/v1/controller/UserV1Controller.java
+++ src/main/java/com/project/hiuni/domain/user/v1/controller/UserV1Controller.java
@@ -1,48 +1,14 @@
 package com.project.hiuni.domain.user.v1.controller;
 
-import com.project.hiuni.domain.auth.entity.SocialProvider;
-import com.project.hiuni.domain.schedule.repository.ScheduleRepository;
-import com.project.hiuni.domain.user.entity.User;
-import com.project.hiuni.domain.user.repository.UserRepository;
-import com.project.hiuni.global.common.dto.response.ResponseDTO;
-import com.project.hiuni.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 public class UserV1Controller {
-
-  private final JwtTokenProvider jwtTokenProvider;
-  private final UserRepository userRepository;
-  private final ScheduleRepository scheduleRepository;
-
-  @GetMapping("/token-test")
-  @Transactional
-  public ResponseDTO<String> tokenTest() {
-
-    User user = User.createStandardUserForSocial("social123", null, "social123@social123", SocialProvider.GOOGLE);
-
-    if(userRepository.findBySocialId("social123").isPresent()) {
-
-      scheduleRepository.deleteAll();
-      scheduleRepository.flush();
-
-      userRepository.deleteBySocialId("social123");
-      userRepository.flush();
-    }
-
-    userRepository.save(user);
-
-    String token = jwtTokenProvider.createAccessToken("social123", "social123");
-
-    return ResponseDTO.of(token, "토큰 발급 성공");
-  }
 
 
 
