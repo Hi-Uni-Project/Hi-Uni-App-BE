1f5402cb9fcc792b4c81023729b8ae9760138176
Hun0914 <bkh010914@naver.com>
2025-12-23 00:09:04 +0900

    refactor : 후기 조회 응답 양식 변경

--- src/main/java/com/project/hiuni/domain/post/dto/response/PostMyReviewResponse.java
+++ src/main/java/com/project/hiuni/domain/post/dto/response/PostMyReviewResponse.java
@@ -1,12 +1,8 @@
 package com.project.hiuni.domain.post.dto.response;
 
-import com.project.hiuni.domain.post.entity.Category;
-import com.project.hiuni.domain.post.entity.Type;
+import java.time.LocalDateTime;
 
-public record PostMyReviewResponse(
-        Category category,
-        Type type,
-        String title,
-        String content
-) {
+public interface PostMyReviewResponse {
+    LocalDateTime startDate();
+    LocalDateTime endDate();
 }
